167724ff7203a1240801717b87eeb9b18d01cb8d
LesLor <leslie.paz.am@gmail.com>
2022-04-26 23:11:22 +0000

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    if str == "":
+        return
+    for i in range(len(str)):
+        a = ""
+        if (i == len(str) - 1):
+            a = "\n"
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            print(f"{chr(ord(str[i]) - 32)}", end=a)
+        else:
+            print("{}".format(str[i]), end=a)
