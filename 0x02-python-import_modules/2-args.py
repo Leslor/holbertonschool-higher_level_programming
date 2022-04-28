b9fb7882feca9170dd4e2bd33ebf5ed480755e53
LesLor <leslie.paz.am@gmail.com>
2022-04-28 20:40:56 +0000

    Task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    """program that prints the number of and the list of its arguments."""
+    import sys
+    args = len(sys.argv) - 1
+    if args == 0:
+        print(f"{args} arguments.")
+    if args == 1:
+        print(f"{args} argument:")
+    else:
+        print(f"{args} arguments:")
+    list_format = [print(f"{i}: {sys.argv[i]}") for i in range(1, args + 1)]
