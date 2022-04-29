3e85b5d1fa409f86b3c1c2afdb7b15f7c689a8ac
LesLor <leslie.paz.am@gmail.com>
2022-04-29 01:56:15 +0000

    Task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    """program that prints all the names
+    defined by the compiled module hidden_4.pyc """
+    import hidden_4
+    list_d = [i for i in dir(hidden_4) if i[0:2] != "__"]
+    print("\n".join(list_d))
