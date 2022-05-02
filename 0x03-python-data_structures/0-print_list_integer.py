cb4ebd7f41021672b5242f805ad31fa84f5cc599
LesLor <leslie.paz.am@gmail.com>
2022-05-02 21:41:44 +0000

    Task-0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,5 @@
 
 def print_list_integer(my_list=[]):
     """Function that prints all integers of a list """
-    print(*my_list, sep='\n')
+    if len(my_list) > 0:
+        print(*my_list, sep='\n')
