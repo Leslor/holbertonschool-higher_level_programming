8f066a3d9a11f122e80ec8793107e8c92b5aea13
LesLor <leslie.paz.am@gmail.com>
2022-05-03 03:44:40 +0000

    task-6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     """Function that print a matrix of integers"""
     if len(matrix) > 0:
         for i in matrix:
-            i = [str(j) for j in i]
-            print(' '.join(i))
+            for j in i:
+                print(f"{j}", end=" ")
+            print("")
