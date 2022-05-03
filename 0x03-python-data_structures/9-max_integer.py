209818f156404090e44d140f36475023351840dc
LesLor <leslie.paz.am@gmail.com>
2022-05-03 04:16:28 +0000

    Task -9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    """Function that finds the biggest integer of a list"""
+    if len(my_list) <= 0:
+        return None
+    max_value = 0
+    for i in my_list:
+        max_value = i if i > max_value else max_value
+    return (max_value)
