8c9cba6a3b31b1b11667dcbc308af77ac5032c07
LesLor <leslie.paz.am@gmail.com>
2022-05-03 04:21:09 +0000

    Task-8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,4 +4,7 @@
 def multiple_returns(sentence):
     """Function that returns a tuple with the length
        of a string and its first character"""
-    return (len(sentence), sentence[0])
+    if len(sentence) != 0:
+        return (len(sentence), sentence[0])
+    else:
+        return (len(sentence), None)
