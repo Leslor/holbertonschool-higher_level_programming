99c83aed067e66de488443229ddf2e7163d38cfb
LesLor <leslie.paz.am@gmail.com>
2022-05-04 22:29:33 +0000

    Task-9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,5 +4,5 @@
 def multiply_by_2(a_dictionary):
     """ function  that returns a new dictionary with values multiplied by 2"""
     values = list(map(lambda value: value * 2, a_dictionary.values()))
-    new_dictionary = dict(zip(a_dictionary.keys(),values))
+    new_dictionary = dict(zip(a_dictionary.keys(), values))
     return new_dictionary
