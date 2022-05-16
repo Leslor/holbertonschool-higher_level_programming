cfe79fc263a9cfb71465fb92d190443c2c65d323
LesLor <leslie.paz.am@gmail.com>
2022-05-16 19:02:11 +0000

    PEP space required around operator

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     """function that divides element by element 2 lists."""
-    new_list=[]
+    new_list = []
     i = 0
     while(i < list_length):
         try:
