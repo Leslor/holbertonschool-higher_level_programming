d9d65f7229124ceb138aaf108bbe76348264a2cd
LesLor <leslie.paz.am@gmail.com>
2022-05-17 23:45:41 +0000

    Task-1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square():
+    """A class Square with its constructor method"""
+    def __init__(self, size):
+        """
+        Args:
+            size: size of Square. Private instance attribute.
+        """
+        self.__size = size
