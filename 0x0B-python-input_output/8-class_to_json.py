bbbdd5a4e5665bdb93ba1f89665952da96d26f08
LesLor <leslie.paz.am@gmail.com>
2022-05-31 23:38:02 +0000

    Task-8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that creates an Object from a “JSON file"""
+import json
+
+
+def class_to_json(obj):
+    """Returns the number of characters added """
+    return  obj.__dict__
