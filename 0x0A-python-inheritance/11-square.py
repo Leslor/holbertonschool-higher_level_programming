781e12c24c8b1bff8908d899387f062b3b657d12
LesLor <leslie.paz.am@gmail.com>
2022-05-31 05:02:42 +0000

    Tas-11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A rectangle"""
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+        self.__width = size
+        self.__height = size
+
+    def __str__(self):
+        return f'[Square] {self.__width}/{self.__height}'
