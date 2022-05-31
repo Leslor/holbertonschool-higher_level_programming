2052c960f2873d96721318da01f7d4f38897898f
LesLor <leslie.paz.am@gmail.com>
2022-05-31 04:03:46 +0000

    Task-7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Class BaseGeometry"""
+""" Class BaseGeometry """
 
 
-class BaseGeometry():
-    """A class Father of the geometrics figures"""
+class BaseGeometry:
+    """ I'm a BaseGeometry class :D """
     def area(self):
-        """Public instance method that raises an Exception
-        the message area() is not implemented"""
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """Public method that validate a value"""
         if type(value) != int:
             raise TypeError(f'{name} must be an integer')
         if value <= 0:
-            raise ValueError(f'{name} must be greater then 0')
+            raise ValueError(f'{name} must be greater than 0')
