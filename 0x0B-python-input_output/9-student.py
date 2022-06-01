3ab6ea51973a008966a12c5a055b2c9fab1054be
LesLor <leslie.paz.am@gmail.com>
2022-06-01 00:56:07 +0000

    Task-9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -11,7 +11,7 @@ class Student():
     """
     def __init__(self, first_name, last_name age):
         """Constructor Method"""
-        self.first_name = name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
