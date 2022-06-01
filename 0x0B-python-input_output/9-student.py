81768691361da23caa39d39c10065a719c3cc589
LesLor <leslie.paz.am@gmail.com>
2022-06-01 00:49:57 +0000

    Task-9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class Student that defines a student """
+
+
+class Student():
+    """Public instance attributes:
+        Args:
+            first_name
+            last_name
+            age
+    """
+    def __init__(self, first_name, last_name age):
+        """Constructor Method"""
+        self.first_name = name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Public method def to_json(self): that
+        retrieves a dictionary representation
+        of a Student instance"""
+        return obj.__dict__
