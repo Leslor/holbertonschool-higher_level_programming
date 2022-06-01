882bc3dfe60dc1a27d28651e1c0c663923a67441
LesLor <leslie.paz.am@gmail.com>
2022-06-01 00:36:12 +0000

    Task-7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,13 +6,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    lista = []
-    for i in range(1, len(sys.argv)):
-        lista.append(sys.argv[i])
     new_lista = load_from_json_file('add_item.json')
 
 except FileNotFoundError:
     save_to_json_file([], 'add_item.json')
 
+lista = []
+for i in range(1, len(sys.argv)):
+    lista.append(sys.argv[i])
 new_lista += lista
 save_to_json_file(new_lista, 'add_item.json')
