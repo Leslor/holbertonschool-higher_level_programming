ec9dd438b14f66906a409b75f2533b680203afbb
LesLor <leslie.paz.am@gmail.com>
2022-06-01 03:25:23 +0000

    Task-11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""class Student that defines a student """
+
+
+class Student():
+    """Public instance attributes:
+        Args:
+            first_name
+            last_name
+            age
+    """
+    def __init__(self, first_name, last_name, age):
+        """Constructor Method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Public method def to_json(self): that
+        retrieves a dictionary representation
+        of a Student instance"""
+        dict_format = {}
+        if type(attrs) == list:
+            for i in attrs:
+                if type(i) != str:
+                    return (self.__dict__)
+            for i in attrs:
+                if i in self.__dict__:
+                    dict_format[i] = self.__dict__[i]
+            return dict_format
+        return (self.__dict__)
+
+    def reload_from_json(self, json):
+        """class Student that defines a studen"""
+        self.__dict__.update(json)
