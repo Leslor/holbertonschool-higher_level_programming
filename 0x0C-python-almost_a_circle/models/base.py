3756eee56f1397713241508701c8ff4d4bf171e3
LesLor <leslie.paz.am@gmail.com>
2022-06-07 00:12:04 +0000

    PEP Style

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,12 +17,14 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """to_json_string method"""
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """save_to_file method"""
         filename = f'{cls.__name__}.json'
         with open(filename, 'w') as f:
             if list_objs is None:
@@ -32,6 +34,7 @@ class Base():
 
     @staticmethod
     def from_json_string(json_string):
+        """from_json_string method"""
         if json_string is None:
             return []
         return json.loads(json_string)
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,7 +4,13 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """class Square that inherits from Rectangle
+    """
+    class Square that inherits from Rectangle
+    Args:
+        size: ...
+        x: ...
+        y: ...
+        id: ...
     """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
@@ -24,9 +30,11 @@ class Square(Rectangle):
         self.height = value
 
     def __str__(self):
+        """__str__ method"""
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.width}'
 
     def update(self, *args, **kwargs):
+        """update method"""
         if len(kwargs) == 0:
             return
         atr_value = {'id': self.id, 'size': self.width,
@@ -39,6 +47,7 @@ class Square(Rectangle):
                       atr_value.get('y'), atr_value.get('id'))
 
     def to_dictionary(self):
+        """to_dictionary method"""
         key_order = ('id', 'x', 'size', 'y')
         return {key_order[i]: getattr(self, key_order[i])
                 for i in range(len(key_order))}
