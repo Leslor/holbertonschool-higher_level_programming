4d0a33d36cd95f764a061fde305fc755901f4b8d
LesLor <leslie.paz.am@gmail.com>
2022-06-07 05:23:40 +0000

    ading the args case

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -35,16 +35,24 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """update method"""
-        if len(kwargs) == 0:
-            return
-        atr_value = {'id': self.id, 'size': self.width,
-                     'x': self.x, 'y': self.y}
-        for key_atr in atr_value.keys():
-            for key, value in kwargs.items():
-                if key_atr == key:
-                    atr_value[key_atr] = value
-        self.__init__(atr_value.get('size'), atr_value.get('x'),
-                      atr_value.get('y'), atr_value.get('id'))
+        if len(args) > 0:
+            atr_va = [self.id, self.size, self.x, self.y]
+            i = 0
+            while i < len(atr_va) and i < len(args):
+                atr_va[i] = args[i]
+                i += 1
+            self.__init__(atr_va[1], atr_va[2], atr_va[3], atr_va[0])
+        elif len(kwargs) > 0:
+            atr_value = {'id': self.id, 'size': self.width,
+                         'x': self.x, 'y': self.y}
+            for key_atr in atr_value.keys():
+                for key, value in kwargs.items():
+                    if key_atr == key:
+                        atr_value[key_atr] = value
+            self.__init__(atr_value.get('size'), atr_value.get('x'),
+                          atr_value.get('y'), atr_value.get('id'))
+    else:
+        return
 
     def to_dictionary(self):
         """to_dictionary method"""
