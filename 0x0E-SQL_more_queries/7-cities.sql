5e89f646ade3fa37e20b85c44df3c725e411f240
LesLor <leslie.paz.am@gmail.com>
2022-07-06 23:02:42 +0000

    Fix: Style

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,9 @@
 -- table cities (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-		PRIMARY KEY (id) INT AUTO_INCREMENT	NOT NULL,
-		state_id		 INT				NOT NULL,
-		name 			 VARCHAR(256) 		NOT NULL,
+		PRIMARY KEY (id) INT			NOT NULL AUTO_INCREMENT,
+		state_id		 INT			NOT NULL,
+		name 			 VARCHAR(256) 	NOT NULL,
 		FOREING KEY(state_id)
-		REFERENCES hbtn_0d_usa.states(id));
+		REFERENCES hbtn_0d_usa.states(id)
+);
