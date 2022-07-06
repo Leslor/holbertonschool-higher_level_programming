0fec9b814f09ac33336a3ab7d950d4c76187f716
LesLor <leslie.paz.am@gmail.com>
2022-07-06 22:30:31 +0000

    Fix: primary key and unique

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,3 @@
 -- script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.state (id INT AUTO_INCREMENT NOT NULL UNIQUE, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.state (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
