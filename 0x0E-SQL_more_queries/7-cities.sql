bf5d1afb4fd46872c224028d33525e7a4faffbb0
LesLor <leslie.paz.am@gmail.com>
2022-07-06 23:00:55 +0000

    Feat: task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,8 @@
 -- table cities (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-		PRIMARY KEY (id) INT AUTO_GENERATE,
-		state_id		 INT,
+		PRIMARY KEY (id) INT AUTO_INCREMENT	NOT NULL,
+		state_id		 INT				NOT NULL,
 		name 			 VARCHAR(256) 		NOT NULL,
-		FOREING KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
+		FOREING KEY(state_id)
+		REFERENCES hbtn_0d_usa.states(id));
