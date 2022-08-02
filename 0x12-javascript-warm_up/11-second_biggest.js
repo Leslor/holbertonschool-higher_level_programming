de00c7ae961c1a77464ead53470868d144f0c515
LesLor <leslie.paz.am@gmail.com>
2022-08-02 22:28:30 +0000

    reverted commit f190eb5b5b71fcac3d57c8bf45257cbf00f10ee

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+if (parseInt(process.argv.length) <= 3) {
   console.log(0);
 } else {
-  const arr = process.argv.slice(2);
-  const max = Math.max.apply(null, arr);
-  arr.splice(arr.indexOf(max), 1);
-  const max2 = Math.max.apply(null, arr);
+  let i, max1, max2;
+  max1 = parseInt(process.argv[2]);
+  max2 = max1;
+  for (i = 3; i < parseInt(process.argv.length); i++) {
+    if (parseInt(process.argv[i]) > max1) {
+      max2 = max1;
+      max1 = parseInt(process.argv[i]);
+    } else if (parseInt(process.argv[i]) > max2) {
+      max2 = parseInt(process.argv[i]);
+    }
+  }
   console.log(max2);
 }
