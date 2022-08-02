8f190eb5b5b71fcac3d57c8bf45257cbf00f10ee
LesLor <leslie.paz.am@gmail.com>
2022-08-02 20:36:57 +0000

    Fix: using the method parseInt

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,14 +3,14 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   let i, max1, max2;
-  max1 = process.argv[2];
+  max1 = parseInt(process.argv[2]);
   max2 = max1;
   for (i = 3; i < process.argv.length; i++) {
-    if (process.argv[i] > max1) {
+    if (parseInt(process.argv[i]) > max1) {
       max2 = max1;
-      max1 = process.argv[i];
-    } else if (process.argv[i] > max2) {
-      max2 = process.argv[i];
+      max1 = parseInt(process.argv[i]);
+    } else if (parseInt(process.argv[i]) > max2) {
+      max2 = parseInt(process.argv[i]);
     }
   }
   console.log(max2);
