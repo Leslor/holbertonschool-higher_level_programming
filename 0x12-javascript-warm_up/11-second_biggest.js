35061b510989ed8c6b41f414daa9caabf3bca8ff
LesLor <leslie.paz.am@gmail.com>
2022-08-02 22:07:57 +0000

    Fix: change the algorithm

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,16 +2,9 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  let i, max1, max2;
-  max1 = parseInt(process.argv[2]);
-  max2 = max1;
-  for (i = 3; i < process.argv.length; i++) {
-    if (parseInt(process.argv[i]) > max1) {
-      max2 = max1;
-      max1 = parseInt(process.argv[i]);
-    } else if (parseInt(process.argv[i]) > max2) {
-      max2 = parseInt(process.argv[i]);
-    }
-  }
+  const arr = process.argv.slice(2);
+  const max = Math.max.apply(null, arr);
+  arr.splice(arr.indexOf(max), 1);
+  const max2 = Math.max.apply(null, arr);
   console.log(max2);
 }
