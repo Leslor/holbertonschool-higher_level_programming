d49ac0cf193a1d49ecada7e8fe7d0fb3de18a334
LesLor <leslie.paz.am@gmail.com>
2022-08-03 02:40:14 +0000

    Fix: typing error

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
-    print() {
-      const X = 'X'.repeat(this.width);
-	  for (let i = 0; i === this.height; i++) {
-        console.log(X);
-	  }
-	}
+  }
+
+  print () {
+    const X = 'X'.repeat(this.width);
+    for (let i = 0; i === this.height; i++) {
+      console.log(X);
+    }
   }
 };
