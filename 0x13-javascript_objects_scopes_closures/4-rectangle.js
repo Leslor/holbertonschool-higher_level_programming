18b8ee448271322641c71e838c4d522693365295
LesLor <leslie.paz.am@gmail.com>
2022-08-03 02:54:00 +0000

    Feat: Task-4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const X = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(X);
+    }
+  }
+
+  rotate () {
+    const temporal = this.width;
+    this.width = this.height;
+    this.height = temporal;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
