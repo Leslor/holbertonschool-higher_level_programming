4822963e55ce95d3eeffe9bc92931561aed3e0dd
LesLor <leslie.paz.am@gmail.com>
2022-08-06 15:00:56 +0000

    Fix: Task-1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''This Script lists all states from the database
+hbtn_0e_0_usa'''
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    def mysqlrun():
+        """Function for connecting to MySQL database"""
+        username, passwd, db_name = argv[1], argv[2], argv[3]
+        try:
+            db_connection = MySQLdb.connect(
+                                host="localhost",
+                                port=3306,
+                                user=username,
+                                passwd=passwd,
+                                db=db_name
+                                )
+        except Exception:
+            return (0)
+
+        cur = db_connection.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+        query = cur.fetchall()
+        for row in query:
+            print(row)
+        cur.close()
+        db_connection.close()
+    mysqlrun()
