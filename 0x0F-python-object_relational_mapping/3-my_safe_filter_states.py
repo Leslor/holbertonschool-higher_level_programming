f9d848f35f9cba373de91d693ef7301b9945806c
LesLor <leslie.paz.am@gmail.com>
2022-08-06 15:38:56 +0000

    Fix: delete try except

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,16 +9,13 @@ from sys import argv
 def mysqlrun():
     """Function for connecting to MySQL database"""
     username, passwd, db_name = argv[1], argv[2], argv[3]
-    try:
-        db_connection = MySQLdb.connect(
-                            host="localhost",
-                            port=3306,
-                            user=username,
-                            passwd=passwd,
-                            db=db_name
-                            )
-    except Exception:
-        return (0)
+    db_connection = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=username,
+                        passwd=passwd,
+                        db=db_name
+                        )
     state = argv[4]
     cur = db_connection.cursor()
     cur.execute(f"SELECT * FROM states "
