476048f28e7b137d58947fd29df2cce0cab4528a
LesLor <leslie.paz.am@gmail.com>
2022-08-06 15:42:19 +0000

    Task-5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''This Script lists all states from the database
+hbtn_0e_0_usa'''
+
+import MySQLdb
+from sys import argv
+import re
+
+
+def mysqlrun():
+    """Function for connecting to MySQL database"""
+    username, passwd, db_name = argv[1], argv[2], argv[3]
+    try:
+        db_connection = MySQLdb.connect(
+                            host="localhost",
+                            port=3306,
+                            user=username,
+                            passwd=passwd,
+                            db=db_name
+                            )
+    except Exception:
+        return (0)
+    state = argv[4]
+    cur = db_connection.cursor()
+    cur.execute("SELECT cities.name FROM cities "
+                "LEFT JOIN states ON cities.state_id = states.id "
+                "WHERE states.name = {}".format(argv[4])
+                "ORDER BY cities.id")
+    query = cur.fetchall()
+    cities = ["".join(re.findall("[a-zA-Z]", str(row))) for row in query]
+    print(", ".join(cities))
+    cur.close()
+    db_connection.close()
+
+
+if __name__ == "__main__":
+    mysqlrun()
