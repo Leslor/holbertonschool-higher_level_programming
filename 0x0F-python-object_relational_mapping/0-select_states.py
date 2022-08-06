df1d14f1bbbe5b9be092188717e3383075508738
LesLor <leslie.paz.am@gmail.com>
2022-08-06 14:50:11 +0000

    Fix:delete try except statement

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,22 +4,20 @@ hbtn_0e_0_usa'''
 
 
 if __name__ == "__main__":
+   """ Connect to the DB and execute a query"""
     import MySQLdb
     from sys import argv
 
     def mysqlrun():
         """Function for connecting to MySQL database"""
         username, passwd, db_name = argv[1], argv[2], argv[3]
-        try:
-            db_connection = MySQLdb.connect(
-                                host="localhost",
-                                port=3306,
-                                user=username,
-                                passwd=passwd,
-                                db=db_name
-                                )
-        except Exception:
-            return (0)
+        db_connection = MySQLdb.connect(
+                            host="localhost",
+                            port=3306,
+                            user=username,
+                            passwd=passwd,
+                            db=db_name
+                            )
 
         cur = db_connection.cursor()
         cur.execute("SELECT * FROM states ORDER BY id")
