39953c333e59d4e8dc1a4631664523ef47bd48ff
LesLor <leslie.paz.am@gmail.com>
2022-08-18 22:21:29 +0000

    Feat: Task-3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Bash script that sends a DELETE request to the URL passed as the first argumen and displays the body of the response
+#Bash script that takes in a URL and displays all HTTP methods the server will accept.
 curl -sI OPTIONS "{$1}"| grep "Allow:" | cut -d ":" -f2| sed -e 's/^[[:space:]]*//'
