f46369bc000bae102147fcdf761ae80db16bde42
LesLor <leslie.paz.am@gmail.com>
2022-08-18 19:35:17 +0000

    Fix: Adding requiremts, just 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#Bash script that takes in a URL, sends a
-#request to that URL, and displays the size
-#of the body of the response
-var=$(curl -sI "${1}" | grep -i "Content-Length:")
-[[ $var =~ [^0-9]+([0-9]+) ]]
-echo "${BASH_REMATCH[1]}"
+#Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+[[ $(curl -sI "${1}" | grep -i "Content-Length:") =~ [^0-9]+([0-9]+) ]]; echo "${BASH_REMATCH[1]}"
