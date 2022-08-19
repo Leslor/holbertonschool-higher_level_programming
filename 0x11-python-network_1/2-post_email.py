bb1e35cd1210004122d6b9a61d09484619ab539c
LesLor <leslie.paz.am@gmail.com>
2022-08-19 18:24:04 +0000

    Fix: adding if__name__==__main___

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,12 +3,13 @@ from sys import argv
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode
 
-url = argv[1]
-value = {'email': argv[2]}
-data = urlencode(value)
-data = data.encode('ascii')
-req = Request(url, data)
-with urlopen(req) as response:
-    the_page = response.read()
-    print("THE PAGE")
-    print(the_page.decode('utf-8'))
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {'email': argv[2]}
+    data = urlencode(value)
+    data = data.encode('ascii')
+    req = Request(url, data)
+    with urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf-8'))
