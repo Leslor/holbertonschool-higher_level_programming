cbc29d2e5078a5453e60fc59c0d72f631ef46393
LesLor <leslie.paz.am@gmail.com>
2022-08-19 19:22:25 +0000

    Feat: Task-4, Fix: task-0 adding format

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script the fetches: https://intranet.hbtn.io/status"""
+
+if __name__ == "__main__":
+    import requests
+
+    url = 'https://intranet.hbtn.io/status'
+    content =  requests.get(url)
+    print('Body response:')
+    print("\t- type: {}".format(type(content.text)))
+    print("\t- content: {}".format(content.text))
+    content.close()
