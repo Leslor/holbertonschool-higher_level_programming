e1550888d5492b6040494edb738c0069507524d2
LesLor <leslie.paz.am@gmail.com>
2022-08-19 18:54:31 +0000

    Feat: Task-3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends
+a request to the URL and displays the body
+of the response (decoded in utf-8)."""
+
+if __name__ == "__main__":
+    from sys import argv
+    from urllib.request import Request, urlopen
+    from urllib.parse import urlencode
+    from urllib.error import HTTPError
+
+    url = argv[1]
+    req = Request(url)
+    try:
+        with urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: ', e.code)
