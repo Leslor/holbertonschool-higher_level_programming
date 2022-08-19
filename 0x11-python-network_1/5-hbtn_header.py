fbbc63606a77f7e0754d9cd831e7c35d10d5a178
LesLor <leslie.paz.am@gmail.com>
2022-08-19 19:35:19 +0000

    Feat: Task-5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL,
+sends a request to the URL and displays
+the value of the variable X-Request-Id in the response headers"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    url = argv[1]
+    content = requests.get(url)
+    print("{}".format(content.headers['X-Request-Id']))
+    content.close()
