befe583570a5b5f18967a8117775bb0514317827
LesLor <leslie.paz.am@gmail.com>
2022-09-14 02:20:28 +0000

    Fix: Semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,8 @@ const axios = require('axios');
 axios
   .get(process.argv[2])
   .then(res => {
-    console.log(`code: ${res.status}`);
+    console.log('code:', res.status);
   })
   .catch(error => {
-    console.error(`code: ${error.response.status}`);
+    console.error('code:', error.response.status);
   });
